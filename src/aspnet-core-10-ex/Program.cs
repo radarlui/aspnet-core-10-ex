741c3cb016b994f053b39d063ac52bff0757e574
radarlui <radarlui@users.noreply.github.com>
2017-03-27 00:05:23 +0800

    Update Program.cs

--- src/aspnet-core-10-ex/Program.cs
+++ src/aspnet-core-10-ex/Program.cs
@@ -16,6 +16,7 @@ namespace aspnet_core_10_ex
                 .UseContentRoot(Directory.GetCurrentDirectory())
                 .UseIISIntegration()
                 .UseStartup<Startup>()
+                .UseUrls("http://*:8080")
                 .Build();
 
             host.Run();
